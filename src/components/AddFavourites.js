5dc4ee3a36124e8dd8e38d30848ecc2fd1465d0c
Mojeed Tosin <mojeedoloruntosin@gmail.com>
2023-11-27 21:43:30 +0100

    Update AddFavourites.js

--- src/components/AddFavourites.js
+++ src/components/AddFavourites.js
@@ -3,7 +3,7 @@ import React from "react";
 const AddFavourite = () => {
   return (
     <>
-      <span className="mr-2">Add to Favourite</span>
+      <span className="mr-2">Add to Favourites</span>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="16"
@@ -14,7 +14,7 @@ const AddFavourite = () => {
       >
         <path
           fill-rule="evenodd"
-          d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"
+          d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
         />
       </svg>
     </>
